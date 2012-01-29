c13d4d7a7fc3e7c9c45c553c346665aef5225778
Robert Kaiser <robert@notebook.box.kairo.at>
2012-01-29 14:54:24 +0100

    turn on debug in production for a bit of experimenting

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var gCanvas, gContext, gGeolocation;
-var gDebug = false;
+var gDebug = true;
 
 var gTileSize = 256;
 var gMaxZoom = 18; // The minimum is 0.
