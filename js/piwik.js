213947ef20fceae30908709b17458f8e690a9bb8
Robert Kaiser <kairo@kairo.at>
2016-12-04 19:27:05 +0100

    add Piwik tracking to Lantea

---
+++ js/piwik.js
@@ -0,0 +1,13 @@
+// Piwik code - licensed under Public Domain.
+  var _paq = _paq || [];
+  _paq.push(["setDomains", ["*.lantea.kairo.at","*.lantea-dev.kairo.at","*.lantea-legacy.kairo.at"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://piwik.kairo.at/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+// End Piwik code
