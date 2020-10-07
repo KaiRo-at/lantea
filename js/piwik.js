da8fd4b697c86d4c61acbe4c95764ab7d27d4198
Robert Kaiser <kairo@kairo.at>
2020-10-08 00:02:54 +0200

    switch to matomo domain as well

--- js/piwik.js
+++ js/piwik.js
@@ -4,7 +4,7 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="https://piwik.kairo.at/";
+    var u="https://matomo.kairo.at/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
